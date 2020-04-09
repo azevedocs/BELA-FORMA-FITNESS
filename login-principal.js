56ba82784f7016e04eb2e3808168ebd4712e8737
FelipeBastosxj <55720172+FelipeBastosxj@users.noreply.github.com>
2020-04-09 11:18:17 -0300

    Primeiro commit-felipe

---
+++ login-principal.js
@@ -0,0 +1,13 @@
+
+/* MAQUINA DE ESCREVER*/
+
+function typeWriter(elemento){
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i )=>{
+        setTimeout(() => elemento.innerHTML += letra, 75 * i)
+    })
+}
+
+const titulo = document.querySelector('.mensagem1')
+typeWriter(titulo)
