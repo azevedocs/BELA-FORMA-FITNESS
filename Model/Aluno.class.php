579b95954f345e115b9bed83c2ddbc92fb2223cc
azevedocs <60318636+azevedocs@users.noreply.github.com>
2020-04-28 12:29:29 -0300

    Add files via upload

---
+++ Model/Aluno.class.php
@@ -0,0 +1,13 @@
+<? php
+ Class Aluno {
+    
+    function  __construct ( $ cpf , $ nome , $ datanascimento , $ endereco , $ telefone ) {
+        $ this -> cpf = $ cpf ;
+        $ this -> nome = $ nome ;
+        $ this -> datanascimento = $ datanascimento ;
+        $ this -> endereco = $ endereco ;
+        $ this -> telefone = $ telefone ;
+    }
+
+}
+?>
